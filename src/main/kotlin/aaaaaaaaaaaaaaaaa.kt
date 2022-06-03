57d2662a43854717dfdf17f1f13330e56748f05f
mehmet <mehmet.imdat0@gmail.com>
2022-06-03 17:05:02 +0300

    ekleme3

--- src/main/kotlin/aaaaaaaaaaaaaaaaa.kt
+++ src/main/kotlin/aaaaaaaaaaaaaaaaa.kt
@@ -1,3 +1,30 @@
 fun main() {
 
 }
+fun main() {1
+
+}fun main() {2
+
+}fun main() {3
+
+}fun main() {4
+
+}fun main() {5
+
+}fun main() {6
+
+
+
+}fun main() {8
+
+fun main() {9
+
+}
+
+
+
+
+
+
+
+
